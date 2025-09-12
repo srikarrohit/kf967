9a2bfdb318e7b07a51af07503e1009c62f47fc32
unknown <srikarrohit@gmail.com>
2025-09-12 15:43:07 +0530

    switch case

--- loops.c
+++ loops.c
@@ -17,11 +17,24 @@ int main(){
         }
     }
     int num;
-    printf("Enter a number between 1 and 10");
+    printf("Enter a number between 1 and 10\n");
     scanf("%d",&num);
     while(num<1 || num>10){
-        printf("Invalid input. Please try again");
+        printf("Invalid input. Please try again\n");
         scanf("%d",&num);
+    }
+    int k=0;
+    do{
+       k++;
+       printf("%d",k);
+    }while(k<5);
+    printf("\n");
+    for(int i=0;i<10;i++){
+        if(i==5){
+            continue;
+            printf("Im a boy");
+        }
+        printf("%d",i);
     }
     return 0;
 }
